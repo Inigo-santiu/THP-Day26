275d17ddb2f0bea4fa3ad38705f30e6a0694ba12
Inigo-santiu <dstavroff@gmail.com>
2020-08-10 17:35:33 +0200

    Validates completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :events
+  has_many :attendances, through :events
+
+  # has_many :events, foreign_key: 'admin_id', class_name: "Event"
+  # has_many :attendances, foreign_key: 'guest_id', class_name: "Attendance"
 end
